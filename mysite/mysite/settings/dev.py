94cd522ff37ab2adba4fe6daa6457db9c46fc592
SandersLin <coolsanderslin@gmail.com>
2021-12-15 16:55:19 +0800

    initialized new wagtail webapp

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-l@^qw*h8w0$j+nckep9#03f=araz&a!mzjoqaz010kd2)5i5p0'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
